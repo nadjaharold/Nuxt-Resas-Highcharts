dfe879f307855f5f6dbde28fbe433de64a8fd3b8
nadjaharold <rus0clov@gmail.com>
2020-08-10 18:18:56 +0900

    success graph rendering

--- components/Graph.vue
+++ components/Graph.vue
@@ -4,21 +4,60 @@
   </div>
 </template>
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import vueHighchartsOptions from '@/mixins/vueHighchartsOptions'
 
 export default {
   name: 'Graph',
   mixins: [vueHighchartsOptions],
+  computed: {
+    ...mapGetters(['getCheckedPrefCodes', 'getResult']),
+  },
+  watch: {
+    getCheckedPrefCodes() {
+      this.load()
+    },
+  },
   mounted() {
     this.initLoad()
   },
   methods: {
+    ...mapActions(['fetchPopulation']),
     initLoad() {
       const lineCharts = this.$refs.lineCharts
       lineCharts.delegateMethod('showLoading', 'Loading...')
       lineCharts.addSeries([])
       lineCharts.removeSeries()
       lineCharts.hideLoading()
+    },
+    load() {
+      const lineCharts = this.$refs.lineCharts
+      lineCharts.delegateMethod('showLoading', 'Loading...')
+      /* データ取得に100~300msほどかかるので念の為400ms待つ。
+       * 取得できなかったらもう一度600ms待って取得を試みる。Loadingを表示することでリスク許容。
+       */
+      setTimeout(() => {
+        lineCharts.removeSeries()
+        const resultSize = this.getResult.length
+        const checkedSize = this.getCheckedPrefCodes.length
+        if (checkedSize <= resultSize) {
+          this.getResult.forEach((item) => {
+            lineCharts.addSeries(item)
+          })
+          lineCharts.hideLoading()
+        } else {
+          setTimeout(() => {
+            const resultSize2 = this.getResult.length
+            const checkedSize2 = this.getCheckedPrefCodes.length
+            if (checkedSize2 <= resultSize2) {
+              this.getResult.forEach((item) => {
+                lineCharts.addSeries(item)
+              })
+            }
+            lineCharts.hideLoading()
+          }, 600)
+        }
+      }, 400)
     },
   },
 }
